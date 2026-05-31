1d6ce86c28e65276f483d6ec87be49608a9903aa
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-05-31 19:36:38 +0300

    feat(home): Add fzf package

--- home.nix
+++ home.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.username = "skarphet";
   home.stateVersion = "25.11";
   home.homeDirectory = "/home/skarphet";
+  home.packages = with pkgs; [
+    fzf
+  ];
 
   imports = [
     ./modules/home/fish/fish.nix
