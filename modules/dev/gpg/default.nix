3f2edd60f582156b7ec323a42d42864990e80cf5
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-11 20:18:06 +0300

    fix(gpg): use the new pinentry package specification style

--- modules/dev/gpg/default.nix
+++ modules/dev/gpg/default.nix
@@ -7,7 +7,7 @@
 
   services.gpg-agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-gnome3;
     defaultCacheTtl = 34560000;
     maxCacheTtl = 34560000;
   };
