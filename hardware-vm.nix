7f36e7e5d78232bb884a2395da53a1d192d3d807
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-11 20:15:12 +0300

    fix(hardware-vm): specify hostPlatform

--- hardware-vm.nix
+++ hardware-vm.nix
@@ -1,6 +1,8 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   fileSystems."/" = {
     device = "/dev/vda1";
