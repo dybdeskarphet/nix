a296b97c1b6b3b2dc6c9cbc50d8ffc4d568b54c0
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-11 05:02:18 +0300

    feat: add fastfetch and easyclone to the home

--- modules/core/home.nix
+++ modules/core/home.nix
@@ -19,6 +19,8 @@ in
     ../utility/bat.nix
     ../utility/streamlink
     ../utility/cava
+    ../utility/easyclone
+    ../utility/fastfetch
     ./env.nix
   ];
 
@@ -27,7 +29,7 @@ in
   # }}}
 
   # Enable programs without config {{{
-  programs.dconf.enable = true;
+  dconf.enable = true;
   # }}}
 }
 
