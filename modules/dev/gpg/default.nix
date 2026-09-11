5780f1e822fc3b10415d838d126987cb1a36331d
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-11 06:20:34 +0300

    fix(gpg): use pinentryPackage instead of pinentry which is a class on its own

--- modules/dev/gpg/default.nix
+++ modules/dev/gpg/default.nix
@@ -7,7 +7,7 @@
 
   services.gpg-agent = {
     enable = true;
-    pinentry = pkgs.pinentry-gnome3;
+    pinentryPackage = pkgs.pinentry-gnome3;
     defaultCacheTtl = 34560000;
     maxCacheTtl = 34560000;
   };
