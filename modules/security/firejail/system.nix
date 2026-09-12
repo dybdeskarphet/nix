cbfc606225baade14923ca39a1b61f034a8355ad
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-12 05:49:41 +0300

    fix(firejail): use key-value logic for programs with different package names

--- modules/security/firejail/system.nix
+++ modules/security/firejail/system.nix
@@ -9,16 +9,37 @@ let
     "yt-dlp"
     "libreoffice"
     "ffmpeg"
-    "obs"
+    { obs = "obs-studio"; }
   ];
+
+  mkWrappedEntry =
+    item:
+    if builtins.isString item then
+      {
+        name = item;
+        value = {
+          executable = "${lib.getExe pkgs.${item}}";
+          profile = "${pkgs.firejail}/etc/firejail/${item}.profile";
+        };
+      }
+    else
+      let
+        binary = builtins.head (builtins.attrNames item);
+        target = item.${binary};
+        pkg = if builtins.isString target then pkgs.${target} else target;
+      in
+      {
+        name = binary;
+        value = {
+          executable = "${lib.getExe pkg}";
+          profile = "${pkgs.firejail}/etc/firejail/${binary}.profile";
+        };
+      };
 in
 {
   programs.firejail = {
     enable = true;
 
-    wrappedBinaries = lib.genAttrs firejailedApps (name: {
-      executable = "${lib.getExe pkgs.${name}}";
-      profile = "${pkgs.firejail}/etc/firejail/${name}.profile";
-    });
+    wrappedBinaries = builtins.listToAttrs (map mkWrappedEntry firejailedApps);
   };
 }
