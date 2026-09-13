d562b49c45bc05d576abde54bd04b8ee906685a8
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-13 05:34:18 +0300

    feat(niri): add niri with all of its config

--- modules/desktop/niri/home.nix
+++ modules/desktop/niri/home.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # xdg.configFile."niri/config.kdl".source = ./config.kdl;
+  home.packages = with pkgs; [
+    jq
+    rofi
+    playerctl
+    libnotify
+    libqalculate
+  ];
+
+  xdg.configFile."niri/config.kdl".source = ./config.kdl;
+  xdg.configFile."niri/src".source = ./src;
+  xdg.configFile."matugen/templates/niri.kdl".source = ./colors.temp.kdl;
 }
