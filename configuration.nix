5b86cc7f67dfa6b7c357c9607bae1db5cdb94660
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-23 21:25:59 +0300

    feat(configuration): add convenience settings for nix

--- configuration.nix
+++ configuration.nix
@@ -1,23 +1,27 @@
-{ ... }:
+{ inputs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true;
+    min-free = 5 * 1024 * 1024 * 1024;
+    http-connections = 50;
+    connect-timeout = 5;
+    stalled-download-timeout = 10;
+  };
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
-  nix.settings = {
-    auto-optimise-store = true;
-    min-free = 5 * 1024 * 1024 * 1024;
-  };
   nix.optimise = {
     automatic = true;
     dates = [ "weekly" ];
   };
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
 
   imports = [
     ./modules/core/system.nix
