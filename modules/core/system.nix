8709c675cc5e65b130b039157b025da77e3e0890
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-26 06:54:36 +0300

    feat(system): add i18n settings

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -32,6 +32,28 @@
   ];
   # }}}
 
+  # I18N {{{
+  i18n.supportedLocales = [
+    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "tr_TR.UTF-8/UTF-8"
+  ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "tr_TR.UTF-8";
+    LC_IDENTIFICATION = "tr_TR.UTF-8";
+    LC_MEASUREMENT = "tr_TR.UTF-8";
+    LC_MONETARY = "tr_TR.UTF-8";
+    LC_NAME = "tr_TR.UTF-8";
+    LC_NUMERIC = "tr_TR.UTF-8";
+    LC_PAPER = "tr_TR.UTF-8";
+    LC_TELEPHONE = "tr_TR.UTF-8";
+    LC_TIME = "tr_TR.UTF-8";
+  };
+  # }}}
+
   # Boot {{{
   boot = {
     initrd = {
